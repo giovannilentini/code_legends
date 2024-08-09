b66cbec2980220f58a3296f5a8186ed7b924072f
giovannilentini <giovannilentini29@gmail.com>
2024-08-09 18:55:47 +0200

    new version of the new challenges page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  get 'challenges/new', to: 'challenges#new', as: 'new_challenge'
-  post 'challenges', to: 'challenges#create'
+  resources :challenges, only: [:new, :create, :show, :index]
 end
+
