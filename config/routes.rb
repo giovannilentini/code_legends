1739ec05e3ae481a59519874178424a97a5170ba
giovannilentini <giovannilentini29@gmail.com>
2024-08-12 12:43:31 +0200

    create links for admin and user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   resources :challenges, only: [:new, :create, :show, :index]
 
+  get 'profile', to: 'profiles#user_profile', as: 'profile'
+  get 'admin_profile', to: 'profiles#admin_profile', as: 'admin_profile'
+
   get 'database_info', to: 'database#info', as: 'info'
 
   post 'clear_tables', to: 'database#clear_tables'
