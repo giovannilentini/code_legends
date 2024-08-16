5b747eb21b43e2305f8f11e575c6f3b16bebf7e4
Edoardo <149409014+Edroedro@users.noreply.github.com>
2024-08-16 12:56:39 +0200

    Aggiunto route per registrazione CONTROLLO
    NON SO SE E' CORRETTO MI PUZZA UN PO'

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/auth/failure' => 'auth0#failure'
   get '/auth/logout' => 'auth0#logout'
   get '/auth/redirect' => 'auth0#redirect'
+  get '/auth/register', to: redirect('https://dev-clyklmmz4z2hbpdz.us.auth0.com/authorize?client_id=F3RKbMB0iW8YTxwmfCg922F0hsio17PC&response_type=code&redirect_uri=http://localhost:3000&scope=openid%20profile%20email&screen_hint=signup')
 
   
 end
