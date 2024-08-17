f0e5ad7b30ab62844450251f7e31a5241aab66db
flavioialongo <flavioialongo@gmail.com>
2024-08-18 01:05:45 +0200

    Rspec initialization and routes changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get "play_now", to: "matchmaking#play_now"
+  get "games", to: "games#index"
+  post "execute_code", to: "games#execute_code"
   post "challenge_friend", to: "matchmaking#challenge_friend"
   post "find_opponent", to: "matchmaking#find_opponent"
 end
