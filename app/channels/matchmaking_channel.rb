bc6e0dc708241c6df1e18956a3e46859ce25ba33
flavioialongo <flavioialongo@gmail.com>
2024-08-18 18:16:23 +0200

    CSS fixed

--- app/channels/matchmaking_channel.rb
+++ app/channels/matchmaking_channel.rb
@@ -1,10 +1,14 @@
 class MatchmakingChannel < ApplicationCable::Channel
   def subscribed
+    p "USER DISCONNECTED"
+    p current_user
     stream_from "matchmaking_#{params[:language]}"
     MatchmakingQueueService.add(current_user, params[:language])
   end
 
   def unsubscribed
+    p "USER DISCONNECTED"
+    p current_user
     MatchmakingQueueService.remove(current_user)
   end
 end
