326c8aea7b9b623786c3bf4ee4dd2fc9660f1b43
giovannilentini <giovannilentini29@gmail.com>
2024-08-30 20:32:12 +0200

    fixed button in create challenge page

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -7,49 +7,27 @@ class ChallengesController < ApplicationController
   end
 
   def create
-
-    ActiveRecord::Base.transaction do
     @challenge = current_user.challenges.new(challenge_params)
 
-    # Filter out invalid test cases
-    valid_test_case_params = test_case_params.reject do |_, params|
-      params[:input_example].blank? || params[:expected_output].blank?
-    end
-
-    valid_test_case_params.each do |index, test_case_params|
-        input_example = test_case_params[:input_example]
-        expected_output = test_case_params[:expected_output]
-        input_type = test_case_params[:input_type]
-        output_type = test_case_params[:output_type]
-        TestCase.create(input_example: input_example, expected_output: expected_output, input_type: input_type, output_type: output_type, challenge: @challenge)
-      end
-    end
     if @challenge.save
-      #redirect_to root_path, notice: 'Sfida creata con successo.'
+      redirect_to @challenge, notice: 'Sfida creata con successo.'
     else
       render :new
     end
   end
 
   def update_status
-    @challenge = Challenge.find(params[:id])
-    puts "Trovato challenge: #{@challenge.inspect}"
-
     @challenge.status = params[:status]
-    puts "Nuovo stato: #{@challenge.status}"
 
     if @challenge.status.to_i == 0
       @challenge.rejection_reason = params[:rejection_reason]
-      puts "Motivo del rifiuto: #{@challenge.rejection_reason}"
     else
       @challenge.rejection_reason = nil
     end
 
     if @challenge.save
-      puts "Salvato con successo"
       redirect_to admin_profile_path, notice: 'Stato aggiornato con successo.'
     else
-      puts "Errore nel salvataggio: #{@challenge.errors.full_messages}"
       redirect_to admin_profile_path, alert: 'Errore nell\'aggiornamento dello stato.'
     end
   end
@@ -62,9 +40,5 @@ class ChallengesController < ApplicationController
 
   def challenge_params
     params.require(:challenge).permit(:description, test_cases_attributes: [:input_example, :expected_output, :input_type, :output_type])
-  end
-
-  def test_case_params
-    params.require(:challenge).permit(test_cases_attributes: [:input_example, :expected_output, :input_type, :output_type])
   end
 end
