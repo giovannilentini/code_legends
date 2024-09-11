c76b058c63b92ed9d7bed4eda5f2799939063d0d
flavioialongo <flavioialongo@gmail.com>
2024-09-11 18:06:32 +0200

    Added editor theme change

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,12 @@
 class WelcomeController < ApplicationController
   before_action :set_languages
   def home
-    unless current_user.guest?
-      @user = current_user
-      @friend_requests = FriendRequest.where(friend_id: @user.id)
-      @challenge_requests = ChallengeRequest.where(friend_id: @user.id)
+    unless current_user.nil?
+      unless current_user.guest?
+        @user = current_user
+        @friend_requests = FriendRequest.where(friend_id: @user.id)
+        @challenge_requests = ChallengeRequest.where(friend_id: @user.id)
+      end
     end
   end
 
