d51a95385c823354115dc05f943d278af2084509
giovannilentini <giovannilentini29@gmail.com>
2024-09-12 17:46:33 +0200

    added new tests

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,45 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe Match, type: :model do
-  let(:player1) { User.create!(auth0_id: 'user1', email: 'example1@gmail.com ')}
-  let(:player2) { User.create!(auth0_id: 'user2', email: 'example2@gmail.com') }
+  describe 'associations' do
+    it 'belongs to player_1' do
+      association = Match.reflect_on_association(:player_1)
+      expect(association.macro).to eq(:belongs_to)
+      expect(association.options[:class_name]).to eq('User')
+      expect(association.options[:foreign_key]).to eq('player_1_id')
+    end
 
-  it "belongs to player_1" do
-    association = Match.reflect_on_association(:player_1)
-    expect(association.macro).to eq(:belongs_to)
-  end
-  it "belongs to player_2" do
-    association = Match.reflect_on_association(:player_2)
-    expect(association.macro).to eq(:belongs_to)
-  end
+    it 'belongs to player_2' do
+      association = Match.reflect_on_association(:player_2)
+      expect(association.macro).to eq(:belongs_to)
+      expect(association.options[:class_name]).to eq('User')
+      expect(association.options[:foreign_key]).to eq('player_2_id')
+    end
 
-  it "is invalid without player_1" do
-    match = Match.new(player_1: nil, player_2: player2)
-    expect(match).to_not be_valid
-    expect(match.errors[:player_1]).to include("can't be blank")
-  end
-    
-  it "is invalid without player_2" do
-    match = Match.new(player_1: player1, player_2: nil)
-    expect(match).to_not be_valid
-    expect(match.errors[:player_2]).to include("can't be blank")
-  end
+    it 'belongs to challenge' do
+      association = Match.reflect_on_association(:challenge)
+      expect(association.macro).to eq(:belongs_to)
+    end
 
-  it "is invalid if player_1 and player_2 are the same" do
-    match = Match.new(player_1: player1, player_2: player1)
-    expect(match).to_not be_valid
-    expect(match.errors[:player_2]).to include("must be different from Player 1")
+    it 'has many chat_messages' do
+      association = Match.reflect_on_association(:chat_messages)
+      expect(association.macro).to eq(:has_many)
+      expect(association.options[:dependent]).to eq(:destroy)
+    end
   end
 
-  it "has many chat_messages" do
-    association = Match.reflect_on_association(:chat_messages)
-    expect(association.macro).to eq(:has_many)
-  end
+  describe 'validations' do
+    let(:user1) { User.create(email: 'user1@example.com', guest: false, password: 'password') }
+    let(:user2) { User.create(email: 'user2@example.com', guest: false, password: 'password') }
+    let(:challenge_proposal) { ChallengeProposal.create(title: 'Challenge Proposal', description: 'Proposal description', test_cases:"asdasd", user: user1) }
+    let(:challenge) { Challenge.create(title: 'Challenge', description: 'Challenge description', challenge_proposal: challenge_proposal) }
 
-  it "destroys associated chat_messages when deleted" do
-    match = Match.create!(player_1: player1, player_2: player2)
-    chat_message = ChatMessage.create!(user: player1, match: match, content: 'Hello')
+    it 'is valid with different players and a challenge' do
+      match = Match.new(player_1: user1, player_2: user2, challenge: challenge)
+      expect(match).to be_valid
+    end
 
-    expect { match.destroy }.to change { ChatMessage.count }.by(-1)
+    it 'is invalid with the same players' do
+      match = Match.new(player_1: user1, player_2: user1, challenge: challenge)
+      match.validate
+      expect(match.errors[:player_2]).to include('must be different from Player 1')
+    end
+
+    it 'is invalid without player_1' do
+      match = Match.new(player_2: user2, challenge: challenge)
+      match.validate
+      expect(match.errors[:player_1]).to include("can't be blank")
+    end
+
+    it 'is invalid without player_2' do
+      match = Match.new(player_1: user1, challenge: challenge)
+      match.validate
+      expect(match.errors[:player_2]).to include("can't be blank")
+    end
+
+    it 'is invalid without a challenge' do
+      match = Match.new(player_1: user1, player_2: user2)
+      match.validate
+      expect(match.errors[:challenge]).to include("can't be blank")
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,120 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context "when not a guest" do
-    let(:user) { User.new(email: email, auth0_id: auth0_id, guest: false, rank: 100) }
-    let(:email) { 'example@gmail.com' }
-    let(:auth0_id) { '123456' }
+  describe '#guest?' do
+    it 'returns true if the user is a guest' do
+      user = User.new(guest: true)
+      expect(user.guest?).to be true
+    end
 
-    it "is valid with unique email and auth0_id" do
-      user.save!
+    it 'returns false if the user is not a guest' do
+      user = User.new(guest: false)
+      expect(user.guest?).to be false
+    end
+  end
+  describe '#admin?' do
+    it 'returns true if the user is an admin' do
+      user = User.new(is_admin: true)
+      expect(user.admin?).to be true
+    end
+
+    it 'returns false if the user is not an admin' do
+      user = User.new(is_admin: false)
+      expect(user.admin?).to be false
+    end
+  end
+  describe '#has_password?' do
+    it 'returns true if the user has a password digest' do
+      user = User.new(password_digest: 'somepassworddigest')
+      expect(user.has_password?).to be true
+    end
+
+    it 'returns false if the user does not have a password digest' do
+      user = User.new(password_digest: nil)
+      expect(user.has_password?).to be false
+    end
+  end
+
+  describe 'email validation' do
+    it 'is valid with a unique email when not a guest' do
+      user = User.new(email: 'test@example.com', guest: false, password_digest: "password_digest")
       expect(user).to be_valid
     end
 
-    it "is invalid without an email" do
-      user.email = nil
-      expect(user).to_not be_valid
+    it 'is invalid without an email when not a guest' do
+      user = User.new(email: nil, guest: false)
+      user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
 
-    it "is invalid with a duplicate email" do
-      User.create!(email: email, auth0_id: '654321', guest: false)
-      user.email = email
-      expect(user).to_not be_valid
-      expect(user.errors[:email]).to include("has already been taken")
-    end
-
-    it "is invalid without an auth0_id" do
-      user.auth0_id = nil
-      expect(user).to_not be_valid
-      expect(user.errors[:auth0_id]).to include("can't be blank")
-    end
-
-    it "is invalid with a duplicate auth0_id" do
-      User.create!(email: 'example2@gmail.com', auth0_id: auth0_id, guest: false)
-      user.auth0_id = auth0_id
-      expect(user).to_not be_valid
-      expect(user.errors[:auth0_id]).to include("has already been taken")
-    end
-
-  it "has many challenge_proposals" do
-    association = User.reflect_on_association(:challenge_proposals)
-    expect(association.macro).to eq(:has_many)
-  end
-
-  it "has one attached profile_image" do
-    expect(user).to respond_to(:profile_image)
-  end
-
-  it "has many sent_friend_requests" do
-    association = User.reflect_on_association(:sent_friend_requests)
-    expect(association.macro).to eq(:has_many)
-  end
-
-  it "has many received_friend_requests" do
-    association = User.reflect_on_association(:received_friend_requests)
-    expect(association.macro).to eq(:has_many)
-  end
-
-  it "has many friendships" do
-    association = User.reflect_on_association(:friendships)
-    expect(association.macro).to eq(:has_many)
-  end
-
-  it "has many friends through friendships" do
-    association = User.reflect_on_association(:friends)
-    expect(association.macro).to eq(:has_many)
-    expect(association.options[:through]).to eq(:friendships)
-  end
-
-  it "has many inverse_friendships" do
-    association = User.reflect_on_association(:inverse_friendships)
-    expect(association.macro).to eq(:has_many)
-  end
-
-  it "has many inverse_friends through inverse_friendships" do
-    association = User.reflect_on_association(:inverse_friends)
-    expect(association.macro).to eq(:has_many)
-    expect(association.options[:through]).to eq(:inverse_friendships)
-    expect(association.options[:source]).to eq(:user)
-  end
-
-  it "has many sent_challenge_requests" do
-    association = User.reflect_on_association(:sent_challenge_requests)
-    expect(association.macro).to eq(:has_many)
-  end
-
-  it "has many received_challenge_requests" do
-    association = User.reflect_on_association(:received_challenge_requests)
-    expect(association.macro).to eq(:has_many)
-  end
-
-  it "decreases rank but not below 0" do
-    user.decrement_rank(50)
-    expect(user.rank).to eq(50)
-    user.decrement_rank(100)
-    expect(user.rank).to eq(0)
-  end
-
-  it "increases rank" do
-    user.increment_rank(20)
-    expect(user.rank).to eq(120)
-  end
-end
-
-
-  context "when a guest" do
-    let(:guest_user) { User.new(email: nil, auth0_id: nil, guest: true) }
-
-    it "is valid without an email" do
-      expect(guest_user).to be_valid
-    end
-
-    it "is valid without an auth0_id" do
+    it 'does not validate email for guest users' do
+      guest_user = User.new(guest: true, email: nil, password_digest: "guest")
       expect(guest_user).to be_valid
     end
   end
+  describe 'associations' do
+    let(:user_1) { User.create!(email: 'test@example.com', guest: false, password_digest: "password_digest")}
+    let(:user_2) { User.create!(email: 'test2@example.com', guest: false, password_digest: "password_digest")}
+    let(:user_3) { User.create!(email: 'test3@example.com', guest: false, password_digest: "password_digest")}
+    it 'can have many sent friend requests' do
+      friend_request1 = FriendRequest.create(user_id: user_1.id, friend_id: user_2.id)
+      friend_request2 = FriendRequest.create(user_id: user_1.id, friend_id: user_3.id)
 
+      expect(user_1.sent_friend_requests.count).to eq(2)
+    end
 
-  
+    it 'can have many received friend requests' do
+      friend_request1 = FriendRequest.create(user_id: user_2.id, friend_id: user_1.id)
+      friend_request2 = FriendRequest.create(user_id: user_3.id, friend_id: user_1.id)
+
+      expect(user_1.received_friend_requests.count).to eq(2)
+    end
+  end
 end
