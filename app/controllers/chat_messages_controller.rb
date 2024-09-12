81114f7294017e85567276a7dff451722dad45ad
giovannilentini <giovannilentini29@gmail.com>
2024-09-12 19:13:42 +0200

    added chat control

--- app/controllers/chat_messages_controller.rb
+++ app/controllers/chat_messages_controller.rb
@@ -5,11 +5,18 @@ class ChatMessagesController < ApplicationController
     @chat_message = @match.chat_messages.new(chat_message_params)
     @chat_message.user = current_user
     authorize! :create, @chat_message
-    if @chat_message.save
-      ChatChannel.broadcast_to @match, render_to_string(partial: 'chat_messages/chat_message', locals: { chat_message: @chat_message })
-      head :ok
+
+    if @chat_message.content == ""
+      flash[:alert] = "Message must be not empty!"
+    elsif @chat_message.content.length > 500
+      flash[:alert] = "Message must be less than 500 characters."
     else
-      head :unprocessable_entity
+      if @chat_message.save
+        ChatChannel.broadcast_to @match, render_to_string(partial: 'chat_messages/chat_message', locals: { chat_message: @chat_message })
+        head :ok
+      else
+        head :unprocessable_entity
+      end
     end
   end
 
