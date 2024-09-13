5b52d98a1f135460eb859ce17d37cdd983f887c6
flavioialongo <flavioialongo@gmail.com>
2024-09-13 20:17:28 +0200

    keys

---
+++ features/signup_and_login.feature
@@ -0,0 +1,10 @@
+Feature: User Registration and Email Confirmation
+
+  Scenario: User registers and confirms their email
+    Given I am on the registration page
+    When I register with valid credentials
+    And I receive an email confirmation link
+    When I visit the confirmation link
+    Then I should see a confirmation success message
+    When I log in with my credentials
+    Then I should see a successful login message
--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,13 +1,13 @@
 # features/step_definitions/user_steps.rb
 
 Given("I am on the registration page") do
-  visit new_user_path
+  visit signup_url
 end
 
 When("I register with valid credentials") do
-  fill_in 'Username', with: 'testuser'
-  fill_in 'Email', with: 'testuser@example.com'
-  fill_in 'Password', with: 'password'
+  fill_in 'user_username', with: 'testuser'
+  fill_in 'user_email', with: 'testuser@example.com'
+  fill_in 'user_password', with: 'password'
   click_button 'Sign Up'
 
   # Retrieve the user and their confirmation token
@@ -15,20 +15,18 @@ When("I register with valid credentials") do
   @confirmation_token = @user.confirmation_token
 end
 
-Then("I should see a message to check my email") do
-  expect(page).to have_content('Please check your email to confirm your account.')
-end
 
 Then("I receive an email confirmation link") do
-  @confirmation_link = confirmation_email_url(token: @confirmation_token)
+  @confirmation_link = confirm_email_url(token: @confirmation_token)
 end
 
 When("I visit the confirmation link") do
   visit @confirmation_link
 end
 
-Then("I should see a confirmation success message") do
-  expect(page).to have_content('Your email has been confirmed.')
+Then("I should see a message to check my email") do
+  # Check for the flash message within the right container or class
+  expect(page).to have_selector('.flash-notice', text: 'Please check your email to confirm your account.')
 end
 
 When("I log in with my credentials") do
@@ -40,10 +38,4 @@ end
 
 Then("I should see a successful login message") do
   expect(page).to have_content('Logged in successfully')
-end
-
-# Helper method to generate the confirmation email URL
-def confirmation_email_url(token:)
-  
-  Rails.application.routes.url_helpers.confi(token: token, host: 'www.example.com')
 end
